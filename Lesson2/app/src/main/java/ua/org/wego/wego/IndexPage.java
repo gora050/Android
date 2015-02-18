d059881efb38d5ba208c4a81438850c90ee71d0b
Ursul_WeGo <gora050@gmail.com>
2015-02-18 10:18:30 +0200

    2 lesson

---
+++ Lesson2/app/src/main/java/ua/org/wego/wego/IndexPage.java
@@ -0,0 +1,42 @@
+package ua.org.wego.wego;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import ua.org.wego.wego.ForecastFragment;
+import android.util.Log;
+
+public class IndexPage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+
+
+    }
+
+
+
+
+}
